f8add85856d498b98f438bd3e0d2a9a6f92287c3
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-09-11 23:55:32 +0530

    Refactor API baseURL assignment to use dynamic hostname

--- Frontend/src/features/home/services/song.api.js
+++ Frontend/src/features/home/services/song.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : '';
+
 const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: baseURL,
     withCredentials: true,
 });
 
